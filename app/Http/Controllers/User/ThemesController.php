98cb3e9e97fca766040997de425ffd0bfa3d35f7
milind <milind@192.168.0.101>
2018-04-16 23:20:00 +0530

    =small fixes

--- app/Http/Controllers/User/ThemesController.php
+++ app/Http/Controllers/User/ThemesController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Shopify\Objects\Theme;
 use Auth;
+use App\User;
 
 class ThemesController extends Controller
 {
     function get_themes(Request $request){
-        return [Auth::user()];
+        return [Auth::user(),Auth::User()];
         $theme = new Theme(Auth::User()->shop_name,Auth::User()->shopify_token);
         
         $themes = $theme->all();
